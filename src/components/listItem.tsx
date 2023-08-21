35ba55e4a66a3d064647e1caca0288e6b013a463
Gwen Bradbury <charmed_89@msn.com>
2023-08-21 22:17:41 +0100

    feat: generates a colour scheme based off an uploaded image

--- src/components/listItem.tsx
+++ src/components/listItem.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ListItem() {
-  return <div>ListItem</div>;
+const copy = <i className="far fa-copy"></i>;
+
+function ListItem({ hex, rgb }) {
+  const [copied, setCopied] = useState(false);
+
+  const copyToClipboard = (e) => {
+    const color = e.target.innerText;
+    navigator.clipboard.writeText(color);
+  };
+
+  return (
+    <li style={{ background: rgb }}>
+      <span
+        onClick={(e) => {
+          copyToClipboard(e);
+          setCopied(true);
+          setTimeout(() => {
+            setCopied(false);
+          }, 1000);
+        }}
+      >
+        {copied ? "Copied!" : hex} {copy}
+      </span>
+    </li>
+  );
 }
 
 export default ListItem;
